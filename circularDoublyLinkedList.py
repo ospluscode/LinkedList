b12d08615d5406a5db0f0104471a7edd89233928
Orkhan Suleymanov <53385236+ospluscode@users.noreply.github.com>
2021-03-10 16:45:47 -0800

    Add files via upload
    Circular Doubly Linked List implementation

---
+++ circularDoublyLinkedList.py
@@ -0,0 +1,159 @@
+# Circular Doubly Linked List implementation
+# Create, Insert, Traverse, Search, Delete operations
+
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class CircularDoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+            if node == self.tail.next:
+                break
+
+
+    #  Create
+    def createCircularDoublyLL(self, nodeValue):
+        newNode = Node(nodeValue)
+        self.head = newNode
+        self.tail = newNode
+        newNode.prev = newNode
+        newNode.next = newNode
+        return "The Circular Doubly LL is created"
+
+
+    # Insert
+    def insertCircularDoublyLL(self, value, location):
+        if self.head is None:
+            return "The Circular Doubly LL does not exist"
+        else:
+            newNode = Node(value)
+            # Beginning
+            if location == 0:
+                newNode.next = self.head
+                newNode.prev = self.tail
+                self.head.prev = newNode
+                self.head = newNode
+                self.tail.next = newNode
+            # End
+            elif location == 1:
+                newNode.next = self.head
+                newNode.prev = self.tail
+                self.head.prev = newNode
+                self.tail.next = newNode
+                self.tail = newNode
+            # Middle
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                newNode.next = tempNode.next
+                newNode.prev = tempNode
+                newNode.next.prev = newNode
+                tempNode.next = newNode
+            return "The node has been inserted"
+
+
+    # Traverse
+    def traverseCircularDoublyLL(self):
+        if self.head is None:
+            print("There is not any node")
+        else:
+            tempNode = self.head
+            while tempNode:
+                print(tempNode.value)
+                if tempNode == self.tail:
+                    break
+                tempNode = tempNode.next
+
+
+    # Reverse traverse
+    def reverseTraverseCircularDoublyLL(self):
+        if self.head is None:
+            print("There is not any node")
+        else:
+            tempNode = self.tail
+            while tempNode:
+                print(tempNode.value)
+                if tempNode == self.head:
+                    break
+                tempNode = tempNode.prev
+
+
+    # Search
+    def searchCircularDoublyLL(self, nodeValue):
+        if self.head is None:
+            return "There is not any node"
+        else:
+            tempNode = self.head
+            while tempNode:
+                if tempNode.value == nodeValue:
+                    return tempNode.value
+                if tempNode == self.tail:
+                    return "The value does not exist"
+                tempNode = tempNode.next
+
+
+    # Delete
+    def deleteNode(self, location):
+        if self.head is None:
+            print("There is not any node")
+        else:
+            # Beginning
+            if location == 0:
+                if self.head == self.tail:
+                    self.head.prev = None
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+                    self.head.prev = self.tail
+                    self.tail.next = self.head
+            # End
+            elif location == 1:
+                if self.head == self.tail:
+                    self.head.prev = None
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.tail = self.tail.prev
+                    self.tail.next = self.head
+                    self.head.prev = self.tail
+            # Middle
+            else:
+                curNode = self.head
+                index = 0
+                while index < location - 1:
+                    curNode = curNode.next
+                    index += 1
+                curNode.next = curNode.next.next
+                curNode.next.prev = curNode
+            print("The node has been deleted")
+
+
+    # Delete Entire
+    def deleteEntireCircularDoublyLL(self):
+        if self.head is None:
+            print("There is not any node")
+        else:
+            self.tail.next = None
+            tempNode = self.head
+            while tempNode:
+                tempNode.prev = None
+                tempNode = tempNode.next
+            self.head = None
+            self.tail = None
+            print("The entire list has been deleted")
